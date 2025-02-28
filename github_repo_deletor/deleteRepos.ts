156b48cb6f982fc204aff947c33b817d97705d8a
AstroWalker24 <nadeemshaik2003@gmail.com>
2025-02-28 11:43:38 +0530

    feat: add repository fetching with error handling for missing credentials

--- github_repo_deletor/deleteRepos.ts
+++ github_repo_deletor/deleteRepos.ts
@@ -3,19 +3,36 @@ import * as dotenv from 'dotenv';
 
 dotenv.config();
 
-const GITHUB_USERNAME = process.env.GITHUB_USERNAME;
-const GITHUB_TOKEN = process.env.GITHUB_TOKEN;
-const API_URL = 'https://api.github.com';
-
-
-// validating the github username and token
-if (!GITHUB_USERNAME || !GITHUB_TOKEN){
-    console.error('Please set GIIHUB_USERNAME AND GITHUB_TOKEN in .env file');
-    process.exit(1);
+if (process.env.GITHUB_USERNAME === undefined || process.env.GITHUB_TOKEN === undefined){
+    throw new Error("Missing GitHub credentials. Please set them in the .env file")
 }
 
 
-// 
+const GITHUB_USERNAME: string = process.env.GITHUB_USERNAME;
+const GITHUB_TOKEN: string  = process.env.GITHUB_TOKEN;
+const API_URL: string = 'https://api.github.com';
+
+
+async function getRepos(): Promise<string[]>{
+    try{
+        const response:any = await axios.get(`${API_URL}/users/${GITHUB_USERNAME}/repos`,{
+            auth:{ username: GITHUB_USERNAME, password: GITHUB_TOKEN }
+        });
+
+        return response.data.map((repo:any)=>{
+            repo.name
+        });
+    }
+
+    catch(error){
+        console.log(`Error Fetching the repositories ${error.message}`);
+        return [];
+    }
+}
+
+
+
+
 
 
 
