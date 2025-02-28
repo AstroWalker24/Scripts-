75acab8af1ad6389845a4cfaaa3d5d0dc15dd260
NADEEM SHAIK <shaiknadeem2003@gmail.com>
2025-02-28 10:47:22 +0530

    Delete github_repo_deletor/deleteRepos.js

--- github_repo_deletor/deleteRepos.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var dotenv = require("dotenv");
-dotenv.config();
-var GITHUB_USERNAME = process.env.GITHUB_USERNAME;
-console.log("Hello");
